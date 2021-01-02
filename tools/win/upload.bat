56baccb3fe237e8e5b158e6f8d008e754c0e871d
Presto Bunny <hello@presto.blog>
2021-01-02 01:07:11 -0600

    Fix Steamworks var

--- tools/win/upload.bat
+++ tools/win/upload.bat
@@ -4,23 +4,23 @@
 :: Version:   0.0.0
 Title "Pushing to itch.io and Steam"
 
-set steamworks=C:\\steamworks\ContentBuilder
+set steamworks=C:\\steamworks\ContentBuilder\Content
 set steamlogin=username
 set steampass=password
-set appscript=app.vdf
-set builddir=ProjectName
-set buildexe=ProjectName
+set steamscript=app.vdf
+set itchbuilddir=ProjectName
+set itchbuildexe=ProjectName
 set itchdry=--dry-run
 set itchname=Username/ProjectName
 
 :: (Steam) All Builds
-%steamworks%\builder\steamcmd.exe +login %steamlogin "%steampass%" +run_app_build "%steamworks%\scripts\%appscript%"
+%steamworks%\builder\steamcmd.exe +login %steamlogin "%steampass%" +run_app_build "%steamworks%\scripts\%steamscript%"
 
 :: (Itch) Windows Build
-butler push %itchdry% "%steamworks%\content\%builddir%" --ignore "%buildexe%.app" --ignore "linux-i686" --ignore "linux-x86_64" --ignore "%buildexe%.sh" --ignore "steam_appid.txt" --ignore "developer" %itchname%:win
+butler push %itchdry% "%steamworks%\%itchbuilddir%" --ignore "%itchbuildexe%.app" --ignore "linux-i686" --ignore "linux-x86_64" --ignore "%itchbuildexe%.sh" --ignore "steam_appid.txt" --ignore "developer" %itchname%:win
 
 :: (Itch) Linux Build
-butler push %itchdry% "%steamworks%\content\%builddir" --fix-permissions --ignore --ignore "windows-i686" --ignore "%buildexe%.exe" --ignore "steam_appid.txt" --ignore "developer" %itchname%:linux
+butler push %itchdry% "%steamworks%\%itchbuilddir" --fix-permissions --ignore --ignore "windows-i686" --ignore "%itchbuildexe%.exe" --ignore "steam_appid.txt" --ignore "developer" %itchname%:linux
 
-:: (Itch) Mac OS Build
-butler push %itchdry% "%steamworks%\content\%builddir%\%buildexe%.app" --fix-permissions --ignore "steam_appid.txt" %itchname%:osx
+:: (Itch) macOS Build
+butler push %itchdry% "%steamworks%\%itchbuilddir%\%itchbuildexe%.app" --fix-permissions --ignore "steam_appid.txt" %itchname%:osx
